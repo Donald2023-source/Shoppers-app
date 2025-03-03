694242191d33672e5b2c11036700c0dc9dfdab2d
Donald2023-source <donalddyusuf@gmail.com>
2025-03-03 08:35:52 -0800

    Commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full h-20 bg-white border-b-[1px] border-b-gray-300'>
+        <div>logo</div>
+        <div>Search Input</div>
+        <div>Navbar list</div>
+    </header>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/Components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <p>This is my shoppers app</p>
+    <div>
+       <Header />
+       <p>This is my shoppers app</p>
+    </div>
+  
   );
 }
