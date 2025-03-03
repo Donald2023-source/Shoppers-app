f3ebf874ebf46f147ba66d38197adf8d5bcd684c
Donald2023-source <donalddyusuf@gmail.com>
2025-03-03 09:06:20 -0800

    Search input

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Container from './Container'
 import Logo from './Logo'
+import SearchInput from './SearchInput'
 
 const Header = () => {
   return (
     <header className='w-full h-20 bg-white border-b-[1px] border-b-gray-300'>
         <Container className='h-full flex items-center justify-between lg:gap-10 gap-5'>
             <Logo />
-            <div>Search Input</div>
+            <SearchInput />
             <div>Navbar list</div>
         </Container>
     </header>
--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const SearchInput = () => {
   return (
-    <div>
-      
+    <div className="w-full hidden md:inline-flex flex-1 h-12">
+      <input
+        className="flex-1 h-full border-[1px] border-black/10 pl-8 pr-28 rounded-lg outline-none bg-transparent placeholder-gray-400 focus:shadow-lg focus:border-none"
+        type="text"
+        placeholder="Search Products..."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
