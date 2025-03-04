5af2eae7ff5fa3f37f446562fdd8aec4e70e9645
Donald2023-source <donalddyusuf@gmail.com>
2025-03-03 20:05:03 -0800

    product and category

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import barner from '../schemas/barner'
+import products from '../schemas/products'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   //barner, Products, category
-  types: [],
+  types: [barner, products],
 }
--- src/sanity/schemas/barner.ts
+++ src/sanity/schemas/barner.ts
@@ -1,14 +1,49 @@
 import { defineField, defineType } from "sanity";
 
 export default defineType({
-    type: 'document',
-    name: 'barner',
-    title: 'Barner',
-    fields: [
-        defineField({
-            name: 'title',
-            title: 'Titile',
-            type: 'string'
-        })
-    ]
-})
+  type: "document",
+  name: "barner",
+  title: "Barner",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Titile",
+      type: "string",
+    }),
+
+    defineField({
+      name: "subtitile",
+      title: "Subtitile",
+      type: "string",
+    }),
+
+    defineField({
+      name: "price",
+      title: "Starting from ",
+      type: "number",
+    }),
+
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      description: "Barner Image",
+      validation: (rule) => rule.required(),
+      options: {
+        hotspot: true,
+      },
+      preview: {
+        select: {
+          imageUrl: "asset.url",
+          title: "caption",
+        },
+      },
+    }),
+  ],
+});
