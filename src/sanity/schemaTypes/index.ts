f2c22c4b76f32fff01a15284077c5264c5b72936
Donald2023-source <donalddyusuf@gmail.com>
2025-03-04 19:06:01 -0800

    Sanity setup

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,9 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import barner from '../schemas/barner'
 import products from '../schemas/products'
+import Category from '../schemas/Category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   //barner, Products, category
-  types: [barner, products],
+  types: [barner, products, Category],
 }
