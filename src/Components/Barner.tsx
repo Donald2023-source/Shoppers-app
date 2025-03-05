c771631ca1d927047b1d1df5214058339a09939c
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 09:04:32 -0800

    Barner

--- src/Components/Barner.tsx
+++ src/Components/Barner.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
-const Barner = () => {
+import { getBarnerData } from "@/lib/getData";
+import React from "react";
+import Container from "./Container";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+const Barner = async () => {
+  const barners = await getBarnerData();
+  const singleBarner = barners[0]
   return (
-    <div>
-       
-    </div>
-  )
-}
+    <Container className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10 md:max-h-[600px]">
+        {/* Left half */}
+      <div>
+        <Image 
+        src={urlFor(singleBarner?.image).url()} 
+        alt={singleBarner.title} 
+        width={500}
+        height={500}
+        />
+      </div>
 
-export default Barner
+      {/* Right hal*/}
+      <div></div>
+    </Container>
+  );
+};
+
+export default Barner;
