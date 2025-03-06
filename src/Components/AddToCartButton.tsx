5ae628235775ff76e2e41c9f4b990181654357d8
Donald2023-source <donalddyusuf@gmail.com>
2025-03-06 12:25:19 -0800

    redux

--- src/Components/AddToCartButton.tsx
+++ src/Components/AddToCartButton.tsx
@@ -18,6 +18,7 @@ const AddToCartButton = ({ item, className }: Props) => {
   };
   return (
     <button
+    onClick={handleAddToCart}
       className={twMerge(
         "bg-black w-full hover:bg-orange-400 hoverEffect cursor-pointer rounded text-white py-2 border border-px border-slate-400 font-semibold tracking-wide flex items-center justify-center gap-1",
         className
