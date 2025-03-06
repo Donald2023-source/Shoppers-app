6ee0dac7b23b14d0b41bb3b9699502a88de65767
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 19:52:13 -0800

    productcard

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -4,15 +4,23 @@ import Link from "next/link";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
 const ProductCard = ({ item }: { item: ProductData }) => {
+  const image = item.image
+  console.log(image)
   return (
     <div className="border border-px border-gray-200 rounded-md relative group iverflow-hidden">
       <div>
         <Link href={`/product/${item?.slug.current}`}>
-            <Image height={500} width={500} className="w-full h-72 object-cover group-hover:scale-105 hoverEffect" src={urlFor(item?.image).url()} alt={item._type} />
+          <Image
+            src={urlFor(item?.image)?.url()}
+            alt={item?._type}
+            width={500}
+            height={500}
+            className="w-full h-72 object-cover group-hover:scale-105 hoverEffect"
+          />
         </Link>
       </div>
     </div>
-  )
+  );
 };
 
 export default ProductCard;
