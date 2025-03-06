16740295ff82cb7b91c9e36a34693e0f8d686079
Donald2023-source <donalddyusuf@gmail.com>
2025-03-06 09:30:02 -0800

    Loader

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -5,13 +5,15 @@ import Container from "@/Components/Container";
 import Barner from "@/Components/Barner";
 import Facilities from "@/Components/Facilities";
 import ProductList from "@/Components/ProductList";
+import Loader from "@/Components/Loader";
 export default function Home() {
   return (
     <Container className="py-10">
        
-       <Barner />
+       {/* <Barner />
       <Facilities />
-      <ProductList />
+      <ProductList /> */}
+      <Loader />
     </Container>
   
   );
