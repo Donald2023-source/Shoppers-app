ef1465823c456bdb8a39304d742bd9dbb2b23563
Donald2023-source <donalddyusuf@gmail.com>
2025-03-06 12:42:48 -0800

    slices

--- src/redux/shoppersSlice.ts
+++ src/redux/shoppersSlice.ts
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { ProductData } from "../../types";
 
-interface UserInfo{
-  id: string,
-  name: string,
-  email: string
+interface UserInfo {
+  id: string;
+  name: string;
+  email: string;
 }
 
 interface InitialState {
-  cart: ProductData[],
-  wishList: ProductData[],
-  UserInfo: UserInfo | null
+  cart: ProductData[];
+  wishList: ProductData[];
+  UserInfo: UserInfo | null;
 }
 const initialState: InitialState = {
   cart: [],
@@ -22,16 +22,70 @@ export const shoppersSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const existingProduct = state.cart.find((item) => item._id === action.payload._id);
-      if(existingProduct) {
-        existingProduct.quantity +=1
+      const existingProduct = state.cart.find(
+        (item) => item._id === action.payload._id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
       } else {
-        state.cart.push(action.payload)
+        state.cart.push(action.payload);
       }
-      
     },
+
+    increaseQuantity: (state, action) => {
+      const existingProduct = state.cart.find(
+        (item) => item._id === action.payload
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      }
+    },
+
+    decreaseQuantity: (state, action) => {
+      const existingProduct = state.cart.find(
+        (item) => item._id === action.payload
+      );
+      if (existingProduct) {
+        existingProduct.quantity -= 1;
+      }
+    },
+
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item._id !== action.payload);
+    },
+
+    resetCart: (state) => {
+      state.cart = [];
+    },
+
+    addToWishList: (state, action) => {
+      const existingProduct = state.cart.find(
+        (item) => item._id === action.payload._id
+      );
+      state.wishList.push(action.payload);
+    },
+    resetWishList: (state) => {
+      state.UserInfo = null;
+    },
+
+    addUser: (state, action) => {
+      state.UserInfo = action.payload
+    },
+    removeUser: (state) => {
+      state.UserInfo = null
+    }
   },
 });
 
-export const { addToCart } = shoppersSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  addToWishList,
+  resetCart,
+  resetWishList,
+  increaseQuantity,
+  decreaseQuantity,
+  addUser, 
+  removeUser
+} = shoppersSlice.actions;
 export default shoppersSlice.reducer;
