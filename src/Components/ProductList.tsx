082413bdcf17f571847115dfd046e8742ec44e9e
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 19:26:46 -0800

    Productlst

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { ProductData } from '../../types'
 import { getProductsData } from '@/lib/getData'
+import products from '@/sanity/schemas/products'
+import ProductCard from './ProductCard'
 
 const ProductList = async() => {
     const Products:ProductData[] = await getProductsData()
-    
+
   return (
     <div>
-      
+      {
+        Products.map((item) => <ProductCard key={item?._id} />)
+      }
     </div>
   )
 }
