834558f2f49264c3e4f6cc9009abeafec066eb92
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 18:55:37 -0800

    facilities

--- src/Components/Facilities.tsx
+++ src/Components/Facilities.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { GoRocket } from 'react-icons/go'
 
 const Facilities = () => {
     const data = [
         {
             title: 'Free delivery',
-            description: 'When ordering above'
+            description: 'When ordering above N200,000',
+            icon: <GoRocket />
+        },
+        {
+            title: 'Secure Payment'
         }
     ]
   return (
