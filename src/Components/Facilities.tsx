43db958db3da88a914c9004d8621384ca1e096e7
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 19:00:42 -0800

    facilities

--- src/Components/Facilities.tsx
+++ src/Components/Facilities.tsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import { FaClockRotateLeft, FaWallet } from 'react-icons/fa6'
-import { GoRocket } from 'react-icons/go'
-import { PiChat } from 'react-icons/pi'
+import React from "react";
+import { FaClockRotateLeft, FaWallet } from "react-icons/fa6";
+import { GoRocket } from "react-icons/go";
+import { PiChat } from "react-icons/pi";
 
 const Facilities = () => {
-    const data = [
-        {
-            title: 'Free delivery',
-            description: 'When ordering above N200,000',
-            icon: <GoRocket />
-        },
-        {
-            title: '90 Days Return',
-            description: 'If goods have problems',
-            icon: <FaClockRotateLeft />
-        },
-        {
-            title: 'Secure Payment',
-            description: "100% secure payment",
-            icon: <FaWallet />
-        },
-        {
-            title: '24/7 Support',
-            description: 'Dedicated support',
-            icon: <PiChat />
-        }
-    ]
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const data = [
+    {
+      title: "Free delivery",
+      description: "When ordering above N200,000",
+      icon: <GoRocket />,
+    },
+    {
+      title: "90 Days Return",
+      description: "If goods have problems",
+      icon: <FaClockRotateLeft />,
+    },
+    {
+      title: "Secure Payment",
+      description: "100% secure payment",
+      icon: <FaWallet />,
+    },
+    {
+      title: "24/7 Support",
+      description: "Dedicated support",
+      icon: <PiChat />,
+    },
+  ];
+  return <div></div>;
+};
 
-export default Facilities
+export default Facilities;
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import Footer from "@/Components/Footer";
 import Container from "@/Components/Container";
 import Barner from "@/Components/Barner";
+import Facilities from "@/Components/Facilities";
 export default function Home() {
   return (
     <Container className="py-10">
        
        <Barner />
-    
+      <Facilities />
     </Container>
   
   );
