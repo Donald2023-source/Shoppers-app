1e95b0cbb633d42262cf81648382674b89d9ce26
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 18:54:37 -0800

    facilities

---
+++ src/Components/Facilities.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Facilities = () => {
+    const data = [
+        {
+            title: 'Free delivery',
+            description: 'When ordering above'
+        }
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Facilities
