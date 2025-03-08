9a75c77409ed807d7c873d7a9a3594be56954b5d
Donald2023-source <donalddyusuf@gmail.com>
2025-03-07 16:05:40 -0800

    Cart

--- src/Components/CartContainer.tsx
+++ src/Components/CartContainer.tsx
@@ -1,14 +1,25 @@
 "use client";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ProductData, StoreState } from "../../types";
 import CartItem from "./CartItem";
+import { resetCart } from "@/redux/shoppersSlice";
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import emptyCart from "../../public/emptyCart.png";
 
 const CartContainer = () => {
   const { cart } = useSelector((state: StoreState) => state?.shoppers);
+  const dispatch = useDispatch();
   const handleReset = () => {
-    
-  }
+    const confirmed = window.confirm("Are you sure you want to reset cart");
+    if (confirmed) {
+      dispatch(resetCart());
+      toast.success("Cart reset successfully");
+    }
+  };
   return (
     <div>
       <div>
@@ -21,16 +32,47 @@ const CartContainer = () => {
               <h2>Sub Total</h2>
             </div>
             <div className="mb-5">
-                {
-                    cart?.map((item: ProductData) => (
-                        <CartItem key={item._id} cart={cart} item={item}/>
-                    ))
-                }
+              {cart?.map((item: ProductData) => (
+                <CartItem key={item._id} cart={cart} item={item} />
+              ))}
             </div>
-            <button className="px-10 cursor-pointer py-3 bg-red-600 rounded-lg text-white mb-4 hover:bg-red-700 ">Reset cart</button>
+            <button
+              onClick={handleReset}
+              className="px-10 cursor-pointer py-3 bg-red-600 rounded-lg text-white mb-4 hover:bg-red-700 "
+            >
+              Reset cart
+            </button>
           </div>
         ) : (
-          <div>No Product</div>
+          <motion.div
+            initial={{ y: 30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+            className="flex flex-col md:flex-row justify-center items-center gap-4 py-32"
+          >
+            <div>
+              <Image
+                src={emptyCart}
+                alt="emptyCart"
+                className="w-80 rounded-lg p-4 mx-auto"
+              />
+            </div>
+            <div className="max-w-[500px] p-4 py-8 bg-white flex flex-col gap-4 items-center rounded-md shadow-lg">
+              <h1 className="text-xl font-bold uppercase">
+                Your Cart feels lonely.
+              </h1>
+              <p className="text-sm text-center px-10 -mt-2">
+                Your Shopping cart lives to serve. Give it purpose - fill it
+                with books, electronics, videos, etc. and make it happy.
+              </p>
+              <Link
+                href={"/"}
+                className="bg-lightOrange text-white hover:bg-darkOrange hoverEffect px-8 py-3 rounded-lg font-semibold"
+              >
+                Continue Shopping
+              </Link>
+            </div>
+          </motion.div>
         )}
       </div>
     </div>
