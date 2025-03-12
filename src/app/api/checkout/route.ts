178ad863afcef58458b9e5e04b4e2c1ff95f52e5
Donald2023-source <donalddyusuf@gmail.com>
2025-03-12 11:08:30 -0700

    payment

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,12 +1,32 @@
+import { ProductData } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export const POST = async (request: NextRequest) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
   try {
-    const reqBody = await request.json()
-    const { items, email} =await reqBody
+    const reqBody = await request.json();
+    const { items, email } = await reqBody;
+
+    const existingItems = items.map((item: ProductData) => ({
+      quantity: item.quantity,
+      price_data: {
+        currency: "ngn",
+        unit_amount: Math.round(item.price * 100),
+        product_data: {
+          name: item?.title,
+          description: item?.description,
+        },
+      },
+    }));
+
+    const origin = request.headers.get("origin");
+    console.log(origin);
+    console.log(reqBody);
     
-    console.log(reqBody)
-    return NextResponse.json({message: "Hello from Checkout"})
+    return NextResponse.json({ message: "Hello from Checkout" });
+
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
